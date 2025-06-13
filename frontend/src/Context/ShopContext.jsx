28a39c1565012ec1748cf386f675a7975abb7d91
Ayub Ali <97268297+yubiszn@users.noreply.github.com>
2025-06-12 20:11:07 -0400

    Add To Cart Functionality, CartItem, and Cart Page

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,38 @@
-import React, { createContext } from "react";
-import all_product from "../Components/Assets/all_product"
+import React, { useState, createContext, useEffect } from "react";
+import all_product from "../Components/Assets/all_product.js"
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let product of all_product) {
+    cart[product.id] = 0;
+  }
+  return cart;
+};
+
+
 const ShopContextProvider = (props)=>{
-  const contextValue = {all_product};
+
+  const [cartItems,setCartItems] = useState(getDefaultCart());
+
+ 
+
+
+  const addToCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+  }
+
+   const removeFromCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+
+   const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+
+ 
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
 
   return (
     <ShopContext.Provider value={contextValue}>
